03cb908e3d094388245744309fa21d83eede3fdb
lariicell <146250529+lariicell@users.noreply.github.com>
2023-11-10 16:36:25 -0400

    aline

---
+++ aline.js
@@ -0,0 +1,23 @@
+const reality = {
+    nome: "Serie",
+    info: {
+    nome: "Lupin",
+    participantes: " Assane Diop, Claire, Raoul, Babakar, Julliete, Youssef. "
+    }
+    };
+    console.log(reality.info); 
+
+    const kardashian = {
+        nome: "Lupin",
+        idade: 43
+        };
+        const Assane = JSON.stringify( Assane );
+        console.log( Raoul ); 
+        const objetoPessoa = JSON.parse( Raoul );
+        console.log(objetoPessoa.nome);
+        var idade = +100;
+        if (idade >= 18) {
+        console.log("Você é maior de idade.");
+        }
+        const Lupinfilho = {
+            nome: "Lupinfilho",
